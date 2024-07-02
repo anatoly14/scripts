cb1fc51f81b4fd84d1adc6662889bf28db36b231
anatoly14 <138898179+anatoly14@users.noreply.github.com>
2024-07-02 23:16:35 +0300

    Optimize code

--- Restart-Hyper-V.ps1
+++ Restart-Hyper-V.ps1
@@ -4,18 +4,12 @@
     foreach ($virtualMachine in $listOfVirtualMachines) {
         Write-Host "Shutting down" $virtualMachine;
         Stop-VM -Name $virtualMachine;
-    }
-    Write-Host "Waiting for 1 minute...";
-    Start-Sleep -Seconds 60;
-    foreach ($virtualMachine in $listOfVirtualMachines) {
         $statusOfVirtualMachine = Get-VM -Name $virtualMachine | Select-Object -ExpandProperty State
-        Write-Host "Status of" $virtualMachine ":" $statusOfVirtualMachine;
         if ($statusOfVirtualMachine -eq "Off") {
-            Write-Host "Starting" $virtualMachine "...";
+            Write-Host "Starting up" $virtualMachine;   
             Start-VM -Name $virtualMachine;
         }
     }
-}
 catch [System.IO.FileNotFoundException] {
     Write-Host "Hyper-V module is not installed!";
 }
