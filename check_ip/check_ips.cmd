b47a02632788e0a0a7fa587729965e63eccd6a6b
anatoly14 <138898179+anatoly14@users.noreply.github.com>
2024-09-13 22:29:31 +0300

    Update check_ips.cmd

--- check_ip/check_ips.cmd
+++ check_ip/check_ips.cmd
@@ -1,27 +1,24 @@
 @echo off
-@chcp 65001
-set /p ScriptLocation=Define a directory where cmd script is located:
-cd %ScriptLocation%
-set /p Choise=Enter 0 if you use ips.txt. Enter 1 if you want to enter ips manually:
-if "%Choise%"=="0" goto endend
-@echo Enter values (one per line), then type "done" on a blank line to finish:
-:loop
-set /p IpAddr=
-if /i "%IpAddr%"=="done" goto end
-echo address %IpAddr% is added
-echo %IpAddr% >> temp.txt
-goto loop
-:end
-for /F "delims=" %%i in (temp.txt) do (
-@echo TIMESTAMP is %date% %time% >> log.txt
-@echo HOST is %%i >> log.txt
-@ping %%i -w 1000 > nul 2>&1 && (@echo %%i is reachable >> log.txt) || (@echo %%i is unreachable >> log.txt)
-@echo. >> log.txt
+chcp 65001 > nul 2>&1
+set /p location=Enter a directory where ips.txt and log.txt files will be located:
+cd %location%
+set /p choice=Enter 0 if you use ips.txt. Enter 1 if you want to enter ips manually:
+if "%choice%"=="0" (
+  if exist ips.txt (
+    goto parse
+  ) else (
+      echo No ips.txt file is found! Please create ips.txt file or select manual input option && exit
+    )
 )
-del temp.txt
-exit
-:endend
-for /F "skip=1" %%i in (ips.txt) do (
+@echo Enter values (one per line), then type "done" on a blank line to finish:
+:setip
+set /p ipaddr=
+if /i "%ipaddr%"=="done" goto parse
+echo address %ipaddr% is added
+echo %ipaddr% >> ips.txt
+goto setip
+:parse
+for /f %%i in (ips.txt) do (
 @echo TIMESTAMP is %date% %time% >> log.txt
 @echo HOST is %%i >> log.txt
 @ping %%i -w 1000 > nul 2>&1 && (@echo %%i is reachable >> log.txt) || (@echo %%i is unreachable >> log.txt)
