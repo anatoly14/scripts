0347772a3adca9d0db5a797a4d2a7041e4c118b4
anatoly14 <138898179+anatoly14@users.noreply.github.com>
2024-09-16 19:05:43 +0300

    refactor: added more dialog options

--- check_ip/check_ips.cmd
+++ check_ip/check_ips.cmd
@@ -1,24 +1,50 @@
 @echo off
 chcp 65001 > nul 2>&1
+set location=
 set /p location=Enter a directory where ips.txt and log.txt files will be located: 2>nul
 if "%location%"=="" (
   echo No directory was specified. Exiting...
   exit /b
 )
 cd /d %location%
+:menu
+echo.
+echo ======================
 echo Select an option:
 echo ips.txt [1]
 echo manual [2]
 echo manual by ip range [3]
-choice /c 123 /n /m "Your decision: "  
+echo remove ips.txt [4]
+echo remove log.txt [5]
+echo exit [6]
+echo ======================
+echo.
+choice /c 123456 /n /m "Your decision: "  
 if %errorlevel% == 1 (
   if exist ips.txt (
     goto parse
   ) else (
-      echo No ips.txt file is found! Please create ips.txt file or select manual input option. Exiting...
-      exit /b
+      echo No ips.txt file is found! Please create ips.txt file or select manual input option.
+      goto menu
     )
 )
+if %errorlevel% == 4 (
+  if exist ips.txt (
+    del /p ips.txt
+    ) else (
+    echo No ips.txt is found in location %location%
+  )
+  goto menu
+)
+if %errorlevel% == 5 (
+  if exist log.txt (
+    del /p log.txt
+  ) else (
+    echo No log.txt in location %location%
+  )
+  goto menu
+)
+if %errorlevel% == 6 exit /b
 echo Enter values (one per line), then type "done" on a blank line to finish:
 :setip
 set /p ipaddr=
