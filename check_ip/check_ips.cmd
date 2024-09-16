fc877cb61bab1e52178a63b4614a543994893edc
anatoly14 <138898179+anatoly14@users.noreply.github.com>
2024-09-16 17:17:51 +0300

    Update check_ips.cmd
    Fix ping writing to log "reachable" for "unreachable" hosts

--- check_ip/check_ips.cmd
+++ check_ip/check_ips.cmd
@@ -2,24 +2,24 @@
 chcp 65001 > nul 2>&1
 set /p location=Enter a directory where ips.txt and log.txt files will be located: 2>nul
 if "%location%"=="" (
-  echo.
   echo No directory was specified. Exiting...
-  timeout /t 3
-  exit
+  exit /b
 )
 cd /d %location%
 echo Select an option:
-echo If you have an ips.txt file with ips to check, enter [1]
-echo If you want to enter ips manually, enter [2]
-choice /c 12 /m "Your decision:"  
-if %errorlevel%==1 (
+echo ips.txt [1]
+echo manual [2]
+echo manual by ip range [3]
+choice /c 123 /n /m "Your decision: "  
+if %errorlevel% == 1 (
   if exist ips.txt (
     goto parse
   ) else (
-      echo No ips.txt file is found! Please create ips.txt file or select manual input option && timeout /t 10 && exit
+      echo No ips.txt file is found! Please create ips.txt file or select manual input option. Exiting...
+      exit /b
     )
 )
-@echo Enter values (one per line), then type "done" on a blank line to finish:
+echo Enter values (one per line), then type "done" on a blank line to finish:
 :setip
 set /p ipaddr=
 if /i "%ipaddr%"=="done" goto parse
@@ -28,8 +28,11 @@ echo %ipaddr% >> ips.txt
 goto setip
 :parse
 for /f %%i in (ips.txt) do (
-@echo TIMESTAMP is %date% %time% >> log.txt
-@echo HOST is %%i >> log.txt
-@ping %%i -w 1000 > nul 2>&1 && (@echo %%i is reachable >> log.txt) || (@echo %%i is unreachable >> log.txt)
-@echo. >> log.txt
+echo TIMESTAMP is %date% %time% >> log.txt
+echo HOST is %%i >> log.txt
+ping %%i -w 1000 | find "TTL" > nul
+if not errorlevel 1 (
+  echo Host %%i is reachable >> log.txt
+  ) else echo Host %%i is unreachable >> log.txt
+echo. >> log.txt
 )
